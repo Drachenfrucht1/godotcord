bbcdf0dc683b2103a09202a2355c2648b651295c
Drachenfrucht1 <dominik.sucker@t-online.de>
2020-11-14 23:17:46 +0100

    Implement functions

--- godotcord_store_manager.cpp
+++ godotcord_store_manager.cpp
@@ -1,5 +1,6 @@
 #include "godotcord_store_manager.h"
 #include "godotcord.h"
+#include "core/func_ref.h"
 
 GodotcordStoreManager *GodotcordStoreManager::singleton = NULL;
 
@@ -11,16 +12,116 @@ void GodotcordStoreManager::_bind_methods() {
 
 }
 
+void GodotcordStoreManager::fetch_skus(Object* p_object, StringName p_funcname) {
+	ERR_FAIL_NULL(p_object);
+
+	FuncRef callback;
+	callback.set_instance(p_object);
+	callback.set_function(p_funcname);
+
+	Godotcord::get_singleton()->get_core()->StoreManager().FetchSkus([this, &callback](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements");
+
+		Array a;
+
+		a.push_back(this->get_skus());
+
+		callback.call_funcv(a);
+	});
+}
+
+Array GodotcordStoreManager::get_skus() {
+	int32_t sku_count = 0;
+	Array ret;
+	Godotcord::get_singleton()->get_core()->StoreManager().CountSkus(&sku_count);
+
+	for (int i = 0; i < sku_count; i++) {
+		discord::Sku sku;
+		Dictionary d;
+		discord::Result result = Godotcord::get_singleton()->get_core()->StoreManager().GetSkuAt(i, &sku);
+		ERR_CONTINUE(result != discord::Result::Ok);
+
+		d["id"] = sku.GetId();
+		d["type"] = (SkuType)sku.GetType();
+		d["name"] = sku.GetName();
+
+		Dictionary price;
+		price["amount"] = sku.GetPrice().GetAmount();
+		price["currency"] = sku.GetPrice().GetCurrency();
+
+		d["price"] = price;
+
+
+		ret.push_back(d);
+	}
+
+	return ret;
+}
+
+void GodotcordStoreManager::fetch_entitlements(Object* p_object, StringName p_funcname) {
+	ERR_FAIL_NULL(p_object);
+
+	FuncRef callback;
+	callback.set_instance(p_object);
+	callback.set_function(p_funcname);
+
+	Godotcord::get_singleton()->get_core()->StoreManager().FetchEntitlements([this, &callback](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements");
+
+		Array a;
+
+		a.push_back(this->get_skus());
+
+		callback.call_funcv(a);
+	});
+}
+
+Array GodotcordStoreManager::get_entitlements() {
+	int32_t sku_count = 0;
+	Array ret;
+	Godotcord::get_singleton()->get_core()->StoreManager().CountEntitlements(&sku_count);
+
+	for (int i = 0; i < sku_count; i++) {
+		discord::Entitlement entitlement;
+		Dictionary d;
+		discord::Result result = Godotcord::get_singleton()->get_core()->StoreManager().GetEntitlementAt(i, &entitlement);
+		ERR_CONTINUE(result != discord::Result::Ok);
+
+		d["id"] = entitlement.GetId();
+		d["type"] = (EntitlementType)entitlement.GetType();
+		d["sku_id"] = entitlement.GetSkuId();
+
+		ret.push_back(d);
+	}
+
+	return ret;
+}
+
+bool GodotcordStoreManager::has_sku_entitlement(int64_t p_sku_id) {
+	bool b;
+	discord::Result result = Godotcord::get_singleton()->get_core()->StoreManager().HasSkuEntitlement(p_sku_id, &b);
+
+	ERR_FAIL_COND_V_MSG(result != discord::Result::Ok, false, "An error occured while checking the sku entitlement");
+
+	return b;
+}
+
+void GodotcordStoreManager::start_purchase(int64_t p_sku_id) {
+	Godotcord::get_singleton()->get_core()->StoreManager().StartPurchase(p_sku_id, [](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while starting the purchase flow");
+	});
+}
+
 void GodotcordStoreManager::init() {
 	Godotcord::get_singleton()->get_core()->StoreManager().FetchEntitlements([](discord::Result result) { ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements") });
 	Godotcord::get_singleton()->get_core()->StoreManager().FetchSkus([](discord::Result result) { ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements") });
 
 	Godotcord::get_singleton()->get_core()->StoreManager().OnEntitlementCreate.Connect([this](discord::Entitlement p_entitlement) {
-		
+		Godotcord::get_singleton()->get_core()->StoreManager().FetchEntitlements([](discord::Result result) { ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements") });
 	});
 
 	Godotcord::get_singleton()->get_core()->StoreManager().OnEntitlementDelete.Connect([this](discord::Entitlement p_entitlement) {
-
+		Godotcord::get_singleton()->get_core()->StoreManager().FetchEntitlements([](discord::Result result) { ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured while fetching user entitlements") });
 	});
 
 }
--- godotcord_store_manager.h
+++ godotcord_store_manager.h
@@ -7,6 +7,22 @@ class GodotcordStoreManager : public Object {
 	GDCLASS(GodotcordStoreManager, Object)
 
 public:
+	enum SkuType {
+		APP = 1,
+		DLC,
+		CONSUMABLE,
+		BUNDLE
+	};
+
+	enum EntitlementType {
+		PURCHASE = 1,
+		PREMIUM_SUBSCRIPTION,
+		DEVELOPER_GIFT,
+		TEST_MODE_PURCHASE,
+		FREE_PURCHASE,
+		USER_GIFT,
+		PREMIUM_PURCHASE,
+	};
 
 protected:
 	static void _bind_methods();
@@ -16,11 +32,19 @@ public:
     static GodotcordStoreManager *get_singleton();
 
 	void fetch_skus(Object *p_object, StringName p_funcname);
+	Array get_skus();
 	void fetch_entitlements(Object *p_objects, StringName p_funcname);
-	void start_purchase(int64_t);
+	Array get_entitlements();
+	bool has_sku_entitlement(int64_t p_sku_id);
+	void start_purchase(int64_t p_sku_id);
+	
 
 
 	void init();
     GodotcordStoreManager();
 };
+
+VARIANT_ENUM_CAST(GodotcordStoreManager::SkuType);
+VARIANT_ENUM_CAST(GodotcordStoreManager::EntitlementType);
+
 #endif
