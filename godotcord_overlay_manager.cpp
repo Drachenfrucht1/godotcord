088bd03059c98550a094e04a3410e645014f4732
Drachenfrucht1 <dominik.sucker@t-online.de>
2020-11-20 21:40:54 +0100

    Implements functions

--- godotcord_overlay_manager.cpp
+++ godotcord_overlay_manager.cpp
@@ -6,6 +6,50 @@ GodotcordOverlayManager *GodotcordOverlayManager::get_singleton() {
 	return GodotcordOverlayManager::singleton;
 }
 
+void GodotcordOverlayManager::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("is_enabled"), &GodotcordOverlayManager::is_enabled);
+	ClassDB::bind_method(D_METHOD("is_locked"), &GodotcordOverlayManager::is_locked);
+	ClassDB::bind_method(D_METHOD("set_locked", "locked"), &GodotcordOverlayManager::set_locked);
+	ClassDB::bind_method(D_METHOD("open_voice_settings"), &GodotcordOverlayManager::open_voice_settings);
+	ClassDB::bind_method(D_METHOD("open_activity_invite", "type"), &GodotcordOverlayManager::open_activity_invite);
+	ClassDB::bind_method(D_METHOD("open_guild_invite", "invite_code"), &GodotcordOverlayManager::open_guild_invite);
+}
+
+bool GodotcordOverlayManager::is_enabled() {
+	bool b;
+	Godotcord::get_singleton()->get_core()->OverlayManager().IsEnabled(&b);
+	return b;
+}
+
+bool GodotcordOverlayManager::is_locked() {
+	bool b;
+	Godotcord::get_singleton()->get_core()->OverlayManager().IsLocked(&b);
+	return b;
+}
+
+void GodotcordOverlayManager::set_locked(bool p_locked) {
+	Godotcord::get_singleton()->get_core()->OverlayManager().SetLocked(p_locked, [](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occures trying set locked of the overlay");
+	});
+}
+
+void GodotcordOverlayManager::open_voice_settings() {
+	Godotcord::get_singleton()->get_core()->OverlayManager().OpenVoiceSettings([](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occures trying to open the voice settings overlay");
+	});
+}
+
+void GodotcordOverlayManager::open_activity_invite(GodotcordActivity::ActivityActionType p_type) {
+	Godotcord::get_singleton()->get_core()->OverlayManager().OpenActivityInvite((discord::ActivityActionType)p_type, [](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured trying to open an activity invite dialog in the overlay");
+	});
+}
+
+void GodotcordOverlayManager::open_guild_invite(String p_invite_code) {
+	Godotcord::get_singleton()->get_core()->OverlayManager().OpenGuildInvite(p_invite_code.utf8(), [](discord::Result result) {
+		ERR_FAIL_COND_MSG(result != discord::Result::Ok, "An error occured trying to open the guild invite dialog in the overlay");
+	});
+}
 
 GodotcordOverlayManager::GodotcordOverlayManager() {
 	ERR_FAIL_COND_MSG(singleton != NULL, "Only one instance of GodotcordOverlayManager should exist");
