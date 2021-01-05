d980e2be4b8998641a1441fdef05cfb90e008556
sairam4123 <sairamkumar2022@gmail.com>
2021-01-05 23:51:58 +0530

    parent ad073dfbe083110e5e7d1b32df7ca6718e378ff0 author sairam4123 <sairamkumar2022@gmail.com> 1609870918 +0530 committer sairam4123 <sairamkumar2022@gmail.com> 1609916932 +0530

--- godotcord_storage_manager.h
+++ godotcord_storage_manager.h
@@ -1,5 +1,9 @@
 #ifndef GODOTCORD_STORAGE_MANAGER_H
+<<<<<<< HEAD
 #define GODGODOTCORD_STORAGE_MANAGER_H
+=======
+#define GODOTCORD_STORAGE_MANAGER_H
+>>>>>>> c4d681b (parent ad073dfbe083110e5e7d1b32df7ca6718e378ff0)
 
 #include "core/object.h"
 #include "godotcord.h"
@@ -8,25 +12,31 @@ class GodotcordStorageManager : public Object {
     GDCLASS(GodotcordStorageManager, Object)
 protected:
     static void _bind_methods();
+    static Dictionary _file_stat(discord::FileStat fileStat) {
+        Dictionary p_stat;
+        p_stat["file_name"] = fileStat.GetFilename();
+        p_stat["last_modified"] = fileStat.GetLastModified();
+        p_stat["size"] = fileStat.GetSize();
+        return p_stat;
+    }
 
 public:
     static GodotcordStorageManager *singleton;
     static GodotcordStorageManager *get_singleton();
 
     String get_path();
-    unsigned char* read(String p_name);
-    void read_async(String p_name, Object* p_object, StringName p_funcname);
-    void read_async_partial(String p_name, int32_t p_offset, int32_t p_length, Object* p_object, StringName p_funcname);
-    void write(String p_name, uint8_t* p_data);
-    void write_async(String p_name, unsigned char* p_data);
+    PoolByteArray read(String p_name);
+    void read_async(String p_name);
+    void read_async_partial(String p_name, int32_t p_offset, int32_t p_length);
+    void write(String p_name, PoolByteArray* p_data);
+    void write_async(String p_name, PoolByteArray* p_data);
     void destroy(String p_name);
-    void exists(String p_name);
-    void stat(String p_name);
+    bool exists(String p_name);
+    Dictionary stat(String p_name);
     uint32_t count();
-    void stat_at(int32_t p_index);
+    Dictionary stat_at(int32_t p_index);
 
-    void init();
     GodotcordStorageManager();
 };
 
-#endif //GGODOTCORD_STORAGE_MANAGER_H
+#endif //GODOTCORD_STORAGE_MANAGER_H
