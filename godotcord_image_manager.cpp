175ce26987a8c5f04242cf243094783570bfd9a4
sairam4123 <kannanscoffee@outlook.com>
2021-06-01 23:03:26 +0530

    Implement get_profile_picture() and rename current get_profile_picture as get_profile_picture_raw()

--- godotcord_image_manager.cpp
+++ godotcord_image_manager.cpp
@@ -8,12 +8,14 @@ GodotcordImageManager *GodotcordImageManager::get_singleton() {
 }
 
 void GodotcordImageManager::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("get_profile_picture", "user_id", "size"), &GodotcordImageManager::get_profile_picture);
+	ClassDB::bind_method(D_METHOD("get_profile_picture_raw", "user_id", "size"), &GodotcordImageManager::get_profile_picture_raw);
+	ClassDB::bind_method(D_METHOD("get_profile_picture", "user_id", "size", "generate_mipmaps"), &GodotcordImageManager::get_profile_picture);
 
-	ADD_SIGNAL(MethodInfo("profile_picture_callback", PropertyInfo(Variant::INT, "user_id"), PropertyInfo(Variant::POOL_BYTE_ARRAY, "image_data")));
+	ADD_SIGNAL(MethodInfo("profile_picture_raw_callback", PropertyInfo(Variant::INT, "user_id"), PropertyInfo(Variant::POOL_BYTE_ARRAY, "image_data")));
+	ADD_SIGNAL(MethodInfo("profile_picture_callback", PropertyInfo(Variant::INT, "user_id"), PropertyInfo(Variant::OBJECT, "texture")));
 }
 
-void GodotcordImageManager::get_profile_picture(int64_t p_user_id, int p_size) {
+void GodotcordImageManager::get_profile_picture_raw(int64_t p_user_id, int p_size) {
 	discord::ImageHandle handle{};
 	handle.SetId(p_user_id);
 	handle.SetSize(p_size);
@@ -34,7 +36,43 @@ void GodotcordImageManager::get_profile_picture(int64_t p_user_id, int p_size) {
 
 						write.release();
 
-						emit_signal("profile_picture_callback", p_user_id, data);
+						emit_signal("profile_picture_raw_callback", p_user_id, data);
+					});
+}
+
+void GodotcordImageManager::get_profile_picture(int64_t p_user_id, int p_size, bool p_generate_mipmaps) {
+	discord::ImageHandle handle{};
+	handle.SetId(p_user_id);
+	handle.SetSize(p_size);
+	handle.SetType(discord::ImageType::User);
+	Godotcord::get_singleton()->get_core()->ImageManager()
+			.Fetch(
+					handle, false, [this, p_user_id, p_generate_mipmaps](discord::Result result, discord::ImageHandle returned_handle) {
+					  ERR_FAIL_COND_MSG(result != discord::Result::Ok, "Something went wrong while requesting the profile picture");
+
+					  discord::ImageDimensions dim{};
+					  Godotcord::get_singleton()->get_core()->ImageManager().GetDimensions(returned_handle, &dim);
+
+					  uint32_t data_size = dim.GetWidth() * dim.GetHeight() * 4;
+					  PoolByteArray data;
+					  data.resize(data_size);
+					  PoolByteArray::Write write = data.write();
+					  Godotcord::get_singleton()->get_core()->ImageManager().GetData(returned_handle, &write[0], data_size);
+					  write.release();
+
+					  PoolByteArray::Read read = data.read();
+
+					  Ref<Image> profile_pic;
+					  profile_pic->create(dim.GetWidth(), dim.GetHeight(), false, Image::Format::FORMAT_RGBA8, data);
+
+					  if (p_generate_mipmaps)
+					  {
+						  profile_pic->generate_mipmaps();
+					  }
+					  Ref<ImageTexture> imageTexture;
+					  imageTexture->create_from_image(profile_pic);
+
+					  this->emit_signal("profile_picture_callback", p_user_id, imageTexture);
 					});
 }
 
